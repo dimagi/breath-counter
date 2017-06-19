4aed2d6ef3e3b95dfb91b447deccc626c66cbccd
Will Pride <wpride@dimagi.com>
2017-06-19 12:32:45 -0400

    init

---
+++ app/src/main/org/commcare/respiratory/C0005R.java
@@ -0,0 +1,42 @@
+package org.commcare.respiratory;
+
+public final class C0005R {
+
+    public static final class attr {
+    }
+
+    public static final class drawable {
+        public static final int ic_launcher = 2130837504;
+        public static final int micon = 2130837505;
+        public static final int rounded = 2130837506;
+    }
+
+    public static final class id {
+        public static final int mainlayout = 2131099648;
+    }
+
+    public static final class layout {
+        public static final int main = 2130903040;
+    }
+
+    public static final class raw {
+        public static final int beep = 2130968576;
+    }
+
+    public static final class string {
+        public static final int app_name = 2131034113;
+        public static final int bpm_label = 2131034120;
+        public static final int breath_button = 2131034115;
+        public static final int breaths_label = 2131034119;
+        public static final int do_not_reset = 2131034124;
+        public static final int hello = 2131034112;
+        public static final int record_button = 2131034117;
+        public static final int reset = 2131034123;
+        public static final int reset_button = 2131034116;
+        public static final int reset_dialogue = 2131034122;
+        public static final int reset_question = 2131034121;
+        public static final int seconds_label = 2131034118;
+        public static final int speak_result = 2131034125;
+        public static final int start_button = 2131034114;
+    }
+}

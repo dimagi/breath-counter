d128e0bd75df9c83b1ebd8623fb8c2b68edce426
Will Pride <wpride@dimagi.com>
2017-08-01 16:36:02 -0400

    rename

---
+++ org.commcare.private/build/generated/source/buildConfig/debug/org/commcare/private/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.commcare.private;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.commcare.private";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "7.0";
+}

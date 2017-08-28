e9d0f14b9564739220acce68bdd79d90d8b3bb4a
Will Pride <wpride@dimagi.com>
2017-08-28 09:20:12 -0400

    set color black

--- org.commcare.respiratory/src/main/java/org/commcare/respiratory/BreathCounterActivity.java
+++ org.commcare.respiratory/src/main/java/org/commcare/respiratory/BreathCounterActivity.java
@@ -208,6 +208,8 @@ public class BreathCounterActivity extends Activity implements OnInitListener {
         this.mBreathButton.setPadding(0, 0, 0, 0);
         this.mResetButton1.setPadding(0, 0, 0, 0);
         this.mResetButton2.setPadding(3, 3, 3, 3);
+        this.mResetButton1.setTextColor(Color.BLACK);
+        this.mResetButton2.setTextColor(Color.BLACK);
         this.mRecordAnswerButton.setPadding(3, 3, 3, 3);
         this.mBreathCountView.setPadding(0, 0, 0, 0);
         this.mTimeView.setPadding(0, 0, 0, 0);
